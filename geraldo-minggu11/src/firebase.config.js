d7f7f62c234ea9e0552ca069f79edbec65366b1b
Geraldo B. I <geraldoarema87@gmail.com>
2020-04-15 16:43:55 +0700

    Tugas minggu 11

---
+++ geraldo-minggu11/src/firebase.config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyC48S730wkBIiY0RGinFuS2fEPMg93UuoQ',
+  authDomain: 'fir-minggu11.firebaseapp.com',
+  databaseURL: 'https://fir-minggu11.firebaseio.com',
+  projectId: 'fir-minggu11',
+  storageBucket: 'fir-minggu11.appspot.com',
+  messagingSenderId: '704110156461',
+  appId: '1:704110156461:web:1f8f706c8bfa732853c424',
+  measurementId: 'G-27GVZ8EYP2',
+};
+
+export default firebaseConfig;
